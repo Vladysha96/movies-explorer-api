42a9e13cb11c0d063f66b9894fdf684acb0236ee
Vlad Kartashov <kazus92@yandex.ru>
2022-12-23 15:31:41 +0300

    Return

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const rateLimit = require('./middlewares/rateLimit');
 const { MONGO_URL } = require('./utils/constants');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 

0b960a6ae45b4c451e3d5df9d577c8b22b8e3699
Vlad Kartashov <kazus92@yandex.ru>
2023-01-14 15:08:23 +0300

    изменил список разрешенных сайтов

--- utils/constants.js
+++ utils/constants.js
@@ -1,12 +1,9 @@
 const MONGO_DB_CODE = 11000;
 const REGEXP_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 const allowedCors = [
-  'http://localhost:3000',
-  'http://localhost:3001',
+  'localhost:3000',
   'https://vladysha96.movies.nomoredomains.club',
   'http://vladysha96.movies.nomoredomains.club',
-  'https://api.vladysha96.movies.nomoredomains.club',
-  'http://api.vladysha96.movies.nomoredomains.club',
 ];
 const MONGO_URL = 'mongodb://127.0.0.1:27017/moviesdb';
 const CAST_ERROR = 'CastError';
